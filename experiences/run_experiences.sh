da6b12774da17e27e887108f0062cfebc6287372
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2025-04-22 22:20:31 +0200

    script Figure 10

--- experiences/run_experiences.sh
+++ experiences/run_experiences.sh
@@ -21,26 +21,77 @@ declare -A params
 
 # Experience Figure 9
 
+#params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
+#params[dego]="-c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
+#
+#alphas=("1")
+#
+#> perf_retwis_fig_9.txt
+#
+#for impl in "${!params[@]}";
+#do
+#    for alpha in "${alphas[@]}";
+#    do
+#        for nbUsersInit in 100000 500000 1000000
+#        do
+#            for nbThread in 1 5 10 20 40 80
+#            do
+#                nbOps=$((1000000*nbThread))
+#                for (( c=1; c<=nbTest; c++ ))
+#                do
+#                    perf=$(perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "${impl}_${nbThread}" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+#                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis_fig_9.txt
+#                done
+#            done
+#        done
+#    done
+#done
+#
+#params=()
+#params[dap]="-c CounterIncrementOnly -s ShardedHashSet -q ShardedLinkedList -m ShardedSkipListMap"
+#
+#for impl in "${!params[@]}";
+#do
+#    for alpha in "${alphas[@]}";
+#    do
+#        for nbUsersInit in 100000 500000 1000000
+#        do
+#            for nbThread in 1 5 10 20 40 80
+#            do
+#                nbOps=$((1000000*nbThread))
+#                for (( c=1; c<=nbTest; c++ ))
+#                do
+#                    perf=$(perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "${impl}_${nbThread}" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z -D | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+#                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis_fig_9.txt
+#                done
+#            done
+#        done
+#    done
+#done
+
+# Experience Figure 10
+
+params=()
 params[juc]="-c juc.Counter -s ConcurrentHashSet -q Queue -m ConcurrentSkipListMap"
 params[dego]="-c CounterIncrementOnly -s ConcurrentHashSet -q QueueMASP -m ExtendedSegmentedSkipListMap"
 
-alphas=("1")
+alphas=("0.01" "0.1" "1")
 
-> perf_retwis.txt
+> perf_retwis_fig_10.txt
 
 for impl in "${!params[@]}";
 do
     for alpha in "${alphas[@]}";
     do
-        for nbUsersInit in 100000 500000 1000000
+        for nbUsersInit in 1000000
         do
             for nbThread in 1 5 10 20 40 80
             do
                 nbOps=$((1000000*nbThread))
                 for (( c=1; c<=nbTest; c++ ))
                 do
-                    perf=$(perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "${impl}_${nbThread}" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
-                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis.txt
+                    perf=$(perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "${impl}_${nbThread}" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z -D | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
+                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis_fig_10.txt
                 done
             done
         done
@@ -54,7 +105,7 @@ for impl in "${!params[@]}";
 do
     for alpha in "${alphas[@]}";
     do
-        for nbUsersInit in 100000 500000 1000000
+        for nbUsersInit in 1000000
         do
             for nbThread in 1 5 10 20 40 80
             do
@@ -62,11 +113,9 @@ do
                 for (( c=1; c<=nbTest; c++ ))
                 do
                     perf=$(perf stat --no-big-num -d -e cache-references,cache-misses,branches,branch-misses,cycles,instructions -o perf.log ./test.sh ${params["${impl}"]} -t Retwis -r "$ratio" -p -e -w $benchmarkTime -u $warmingUpTime -h "${impl}_${nbThread}" -y $nbUsersInit -d $nbUsersInit -i $nbOps -b -g $nbThread -A $alpha -z -D | grep -i "completion time :" | awk '{print $6}' | sed s/\(//g)
-                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis.txt
+                    echo ${impl}";"${alpha}";"${nbUsersInit}";"${nbThread}";"${perf} >> perf_retwis_fig_10.txt
                 done
             done
         done
     done
 done
-
-alphas=("0.01" "0.1" "1")
