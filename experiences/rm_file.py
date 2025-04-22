ac42b90ccdf970dbbd29dfe1855257e295d1d040
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2025-04-23 00:52:45 +0200

    update

--- experiences/rm_file.py
+++ experiences/rm_file.py
@@ -1,19 +1,19 @@
 import sys
 import os
 
-benchmark_type = sys.argv[1]
-type_obj = sys.argv[2]
-if benchmark_type == 'Retwis':
-    nb_user = sys.argv[3]
-    completion_time = sys.argv[4]
-
-if benchmark_type == "Retwis":
-    if completion_time ==  "True":
-        list_op = ["ALL"]
-    else:
-        list_op = ["ALL", "ADD", "FOLLOW", "UNFOLLOW", "TWEET", "READ", "COUNT","GROUPE","PROFILE","avg_time_computed"]#, "avg_queue_size", "avg_Follower", "avg_Following", "proportion_Max_Follower", "proportion_Max_Following","proportion_User_With_Max_Following", "proportion_User_With_Max_Follower", "proportion_User_Without_Follower","proportion_User_Without_Following", "nb_user_final", "nb_tweet_final"]
-else:
-    list_op = ["ALL", "ADD", "REMOVE", "READ"]
+# benchmark_type = sys.argv[1]
+# type_obj = sys.argv[2]
+# if benchmark_type == 'Retwis':
+#     nb_user = sys.argv[3]
+#     completion_time = sys.argv[4]
+#
+# if benchmark_type == "Retwis":
+#     if completion_time ==  "True":
+#         list_op = ["ALL"]
+#     else:
+#         list_op = ["ALL", "ADD", "FOLLOW", "UNFOLLOW", "TWEET", "READ", "COUNT","GROUPE","PROFILE","avg_time_computed"]#, "avg_queue_size", "avg_Follower", "avg_Following", "proportion_Max_Follower", "proportion_Max_Following","proportion_User_With_Max_Following", "proportion_User_With_Max_Follower", "proportion_User_Without_Follower","proportion_User_Without_Following", "nb_user_final", "nb_tweet_final"]
+# else:
+#     list_op = ["ALL", "ADD", "REMOVE", "READ"]
 
 # for op in list_op:
 #     try:
