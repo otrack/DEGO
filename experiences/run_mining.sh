05aa92e421930b5c156691dff14bf18206f762cb
Boubacar Kane <Boubacar.Kane@telecom-sudparis.eu>
2025-04-30 09:46:22 +0200

    test mining.py

--- experiences/run_mining.sh
+++ experiences/run_mining.sh
@@ -70,14 +70,14 @@ done
 for repo in "${repos[@]}"
 do
     echo "Analyzing repository: ${repo}"
-    python3.11 mining.py -r "${repo}" ${class_args} -hot
+#    python3.11 mining.py -r "${repo}" ${class_args} -hot
     python3.11 mining.py -r "${repo}" ${class_args} -e
-    python3.11 mining.py -r "${repo}" ${class_args}
+#    python3.11 mining.py -r "${repo}" ${class_args}
 done
 
-python3.11 sort_hot_file.py
-python3.11 generate_matrice.py
-#
+#python3.11 sort_hot_file.py
+#python3.11 generate_matrice.py
+
 #for clazz in "${classes[@]}"
 #do
 #    class_args+="-c ${clazz} "
